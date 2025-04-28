239eddcb7190b131e7297d6d7b3559b3ff41543c
sashavrg <168757785+sashavrg@users.noreply.github.com>
2025-04-28 11:57:56 +0200

    Configured app.js and index

---
+++ Backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const itemsRouter = require('./controllers/items')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('conmnected to MongoDB', error.message)
+  })
+
+  app.use(express.static('dist'))
+  app.use(express.json())
+  app.use(middleware.requestLogger)
+
+  app.use('/api/items', itemsRouter)
+
+  app.use(middleware.unknownEndpoint)
+  app.use(middleware.errorHandler)
+
+  module.export = app
